3f6d646610418d29fc42818d9f700dc503444bf0
Jason.json <88512392+Just-a-Jason@users.noreply.github.com>
2023-12-02 22:16:42 +0100

    ChatGPTWindowDisplayFix

--- extension/Modules/ChatGPT/ChatGPT.ts
+++ extension/Modules/ChatGPT/ChatGPT.ts
@@ -9,32 +9,35 @@ export default abstract class ChatGPT {
     private static _window: ResponseWindow | null = null;
 
     static turn(state:boolean) {
-        if (!state) {
-            ChatGPT._window?._remove();
-            ChatGPT._window = null;
+        if(document.URL.includes('/exam/DoTestQuestion.html') || document.URL.includes('/exam/DoStartTest.html') || document.URL.includes('/exam/LoadQuestion.html')) {
+
+            if (!state) {
+                ChatGPT._window?._remove();
+                ChatGPT._window = null;
+            }
+            else {
+                ChatGPT._window = new ResponseWindow();
+                const parent = document.querySelector('.question-area') as HTMLElement;
+                DomInserter.insert(ChatGPT._window, parent);
+    
+                console.log('%cChatGPT module has been initialized.', 'color:#80d724; font-size: 20px;');
+    
+                const questionContent = QuestionReader.readQuestionText() + '\n' + QuestionReader.readAnswers();
+    
+                console.log(questionContent);
+    
+                setTimeout(() => {
+                    const body = ChatGPT._window?._getBody()?.querySelector('.ChatGPTResponse__Body');
+                    if(body) {
+                        const error = new ChatGPTError('Something went wrong. Please try again later.');
+                        const button = new RetryButton();
+    
+                        HtmlHelper.removeAllChild(body as HTMLElement);
+                        body.appendChild(error._getBody());
+                        body.appendChild(button._getBody());
+                    }
+                }, 3000);
+            };
         }
-        else {
-            ChatGPT._window = new ResponseWindow();
-            const parent = document.querySelector('.question-area') as HTMLElement;
-            DomInserter.insert(ChatGPT._window, parent);
-
-            console.log('%cChatGPT module has been initialized.', 'color:#80d724; font-size: 20px;');
-
-            const questionContent = QuestionReader.readQuestionText() + '\n' + QuestionReader.readAnswers();
-
-            console.log(questionContent);
-
-            setTimeout(() => {
-                const body = ChatGPT._window?._getBody()?.querySelector('.ChatGPTResponse__Body');
-                if(body) {
-                    const error = new ChatGPTError('Something went wrong. Please try again later.');
-                    const button = new RetryButton();
-
-                    HtmlHelper.removeAllChild(body as HTMLElement);
-                    body.appendChild(error._getBody());
-                    body.appendChild(button._getBody());
-                }
-            }, 3000);
-        };
     }
 } 
