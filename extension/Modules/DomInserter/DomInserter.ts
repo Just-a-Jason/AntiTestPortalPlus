397cb56fbea8952efa80aacd6f9a7f6b1cb5a899
Jason.json <88512392+Just-a-Jason@users.noreply.github.com>
2024-01-22 01:07:15 +0100

    parseUIComponent() function

--- extension/Modules/DomInserter/DomInserter.ts
+++ extension/Modules/DomInserter/DomInserter.ts
@@ -1,9 +1,9 @@
+import { parseUIComponent } from "../Helpers/HtmlHelper/HtmlHelper";
 import UIComponent from "../UI Components/UIComponent";
 
 export default abstract class DomInserter {
     static insert(element: HTMLElement | UIComponent, _parent: null | HTMLElement | UIComponent = null): void {
-        element = (element instanceof UIComponent) ? element._getBody().element : element;
-        if(element instanceof UIComponent) element = element._getBody().element;
+        element = (element instanceof UIComponent) ? parseUIComponent(element) : element;
         
         if (_parent === null) document.body.append(element);
         else if (_parent instanceof UIComponent) _parent.appendChild(element);
